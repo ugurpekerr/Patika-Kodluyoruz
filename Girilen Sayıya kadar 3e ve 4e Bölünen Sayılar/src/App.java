961bd4e5c47cd1051e238fe4126bda4de8569dcb
ugur.peker <uur.peker55@gmail.com>
2023-07-09 16:01:09 +0300

    Kodlar ilk kez yazıldı.

---
+++ Girilen Sayıya kadar 3e ve 4e Bölünen Sayılar/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i, say = 0, top = 0, num;
+        double ort;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        num = inp.nextInt();
+
+        for (i = 1; i <= num; ++i) {
+            if ((i %4 == 0) || (i %3 == 0)) {
+                say++;
+                top += i;
+                System.out.println(i);
+            }
+        }
+        ort = top / say;
+        System.out.println("Ort = " + ort);
+        inp.close();
+    }
+}
