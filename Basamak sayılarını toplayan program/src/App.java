e271cad7875f4e5b704c36a186c78b12f2155bec
ugur.peker <uur.peker55@gmail.com>
2023-07-15 18:10:23 +0300

    kod çalışmıyordu düzenlendi

--- Basamak sayılarını toplayan program/src/App.java
+++ Basamak sayılarını toplayan program/src/App.java
@@ -5,7 +5,7 @@ public class App {
         Scanner inp = new Scanner(System.in);
         System.out.print("Sayı giriniz: ");
         int number = inp.nextInt();
-        int tempNumber = number, numberCounter = 0, basTop = 0;
+        int tempNumber = number, numberCounter = 0, basTop = 0, basValue;
 
         while (number != 0) {
             number /= 10;
@@ -14,10 +14,12 @@ public class App {
         System.out.println("Basamak Sayısı: " + numberCounter);
 
         number = tempNumber;
-        for (int i=0; i<numberCounter; i++) {
-            number %= 10;
-            basTop+=number;
+        for (int i=0; i<numberCounter; i++, number/=10) {
+            basValue = number % 10;
+            System.out.println("Sayı:" + number + " Son Basamak:" + basValue);
+            basTop+=basValue;
         }
-        System.out.println("Basamak Sayısı Toplamı : " + basTop);
+        System.out.println("Basamak Sayıları Toplamı: " + basTop);
+        inp.close();
     }
 }
