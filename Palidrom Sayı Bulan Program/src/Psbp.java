ee3041470c419c547de658a07ab9eb5c66f0fc73
ugur.peker <uur.peker55@gmail.com>
2023-08-07 21:15:00 +0300

    first

---
+++ Palidrom Sayı Bulan Program/src/Psbp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Psbp {
+    static boolean isPalidrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber*10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalidrom(99999999));
+    }
+}
